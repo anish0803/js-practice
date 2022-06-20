fdaf695341d3b7f4ed4bb256ea8ebf91f7fe29cd
anish0803 <singhanish124@gmail.com>
2022-06-20 22:01:02 +0530

    sum of cube

---
+++ sumofcube.js
@@ -0,0 +1,12 @@
+function sumofsquare (num){
+    let sum =0;
+while(num!=0){
+    let rem = num%10;
+    sum += rem ** 3;
+    num= Math.floor(num/10);
+}
+return sum;
+}
+let ans = sumofsquare (49);
+
+console.log(ans);
