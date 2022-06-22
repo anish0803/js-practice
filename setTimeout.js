ee9c7991681e7827a4d1289bc39a8b68f19bb79f
anish0803 <singhanish124@gmail.com>
2022-06-22 17:10:06 +0530

    settimeout function

---
+++ setTimeout.js
@@ -0,0 +1,6 @@
+// ----------setTimeout function-----print hello anish in 3 second to use in setTimeout function------
+
+function a (){
+    console.log("hello anish");
+}
+setTimeout(a,3000);      // "a" is  called a callback function
