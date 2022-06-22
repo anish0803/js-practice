ba58bfd8fcbe68044360bce01fc2348cb1156443
anish0803 <singhanish124@gmail.com>
2022-06-22 17:11:08 +0530

    setinterval function

---
+++ setinterval.js
@@ -0,0 +1,10 @@
+// ----------setInterval function---,
+
+function a (){
+    console.log("hello anish");
+}
+setInterval(a,3000);
+
+
+
+// it will print in every 3 seconds print hello anish to use of the setinterval function.
