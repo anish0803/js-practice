ef324a8dfb422fe1c8046d275756266a18bd0f56
anish0803 <singhanish124@gmail.com>
2022-06-23 13:43:07 +0530

    self intro commit using settime

---
+++ single_thread1.js
@@ -0,0 +1,10 @@
+// --------single thread---------
+
+
+console.log("Hello");
+function x (){
+    console.log("I want to be web developer");
+}
+setTimeout(x,2000);
+console.log("My Goal");
+console.log("I Am Anish Singh");
